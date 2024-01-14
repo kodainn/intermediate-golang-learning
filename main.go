12e3e2186e865497fb5c99f733eb27ca763312ae
せみころん <kodai030513@gmail.com>
2024-01-14 17:51:07 +0900

    アーキテクチャの改良について学んだ

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/kodainn/repository/handlers"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kodainn/repository/api"
+)
+
+var (
+	dbUser     = "docker"
+	dbPassword = "pass"
+	dbDatabase = "sampledb"
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
 func main() {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r := api.NewRouter(db)
 
 	log.Println("server start at port 8081")
 	log.Fatal(http.ListenAndServe(":8081", r))
